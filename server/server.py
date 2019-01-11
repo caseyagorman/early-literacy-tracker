c5f8f0f2575fff67c5c11316dfe0b6d2a3b00079
Casey Gorman <caseyagorman@gmail.com>
2019-01-11 01:19:27 -0800

    add user routes to server

--- server/server.py
+++ server/server.py
@@ -1,18 +1,76 @@
 from flask import (Flask, jsonify, request, session)
 from jinja2 import StrictUndefined
 from flask_cors import CORS, cross_origin
+from flask_restful import  Api
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
 from functools import wraps
 from model import Student, Word, Sound, StudentWord, StudentWordTestResult, StudentLetterTestResult, StudentSoundTestResult, Letter, StudentLetter, StudentSound, connect_to_db, db, User
-
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(
+                public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid'})
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+
+@app.route("/api/register", methods=['POST'])
+@cross_origin()
+def add_user():
+    data = request.get_json()
+    print("user data", data)
+ 
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    hashed_password = generate_password_hash(password)
+    existing_user_name = User.query.filter_by(username=username).first()
+    existing_user_email = User.query.filter_by(email=email).first()
+    if existing_user_name:
+        return jsonify({'error': 'username already in use'})
+    if existing_user_email:
+        return jsonify({'error': 'email already in use'})
+    new_user = User(public_id=str(uuid.uuid4()), username=username, email=email,
+                    password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'newUser': 'user added'})
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    auth_user = User.query.filter_by(username=username).first()
+    if not auth_user:
+        return jsonify({'error': 'user does not exist'})
+    if auth_user and check_password_hash(auth_user.password, password.encode('utf-8')):
+        token = jwt.encode({'public_id': auth_user.public_id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(hours=24)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('utf-8'), 'username': auth_user.username})
+    else:
+        return jsonify({'error': 'incorrect password'})
+
 
 
 if __name__ == "__main__":
