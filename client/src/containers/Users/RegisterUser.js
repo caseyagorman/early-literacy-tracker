2a70c3cd55e247092e9bc74f46a12f8f9153903e
Casey Gorman <caseyagorman@gmail.com>
2019-01-11 18:31:39 -0800

    add redux folders

---
+++ client/src/containers/Users/RegisterUser.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RegisterUser extends Component {
+  render() {
+    return <div>welcome!</div>;
+  }
+}
+
+export default RegisterUser;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import RegisterUser from "./containers/Users/RegisterUser";
+import history from "./history";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store/configureStore";
+const initialState = {};
+const store = configureStore(initialState);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const AppRouter = () => (
+  <Router history={history}>
+    <div>
+      <Route path="/register" component={RegisterUser} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
