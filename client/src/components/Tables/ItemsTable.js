2e0e43eb2f2fb6d510c7aa06036f8be9d6573088
Casey Gorman <caseyagorman@gmail.com>
2019-01-14 18:46:43 -0800

    add items table

---
+++ client/src/components/Tables/ItemsTable.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Display table head of words students are learning
+const noBulletList = { listStyleType: "none" };
+const listElements = el => <li>{el}</li>;
+
+const ItemsTable = (items, onSort, route) => (
+  <table bordered="true" hover="true">
+    {console.log("table items", items)}
+    <thead>
+      <tr>
+        <th onClick={e => onSort(e, "item")}> Item</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map(function(item) {
+        return (
+          <tr>
+            <td key={item.item_id}>
+              <Link to={`/item-detail/${item.item_id}`} className="link">
+                <h2 id="item-name-table-header">{item.item}</h2>
+              </Link>
+            </td>
+          </tr>
+        );
+      })}
+      <tr>
+        <td colspan="8">
+          <a href={route} className="link">
+            + Click to add new item
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+export default ItemsTable;
