2161f339aa8a780841f26eb04ced20ab751bde75
Casey Gorman <caseyagorman@gmail.com>
2019-01-21 11:22:32 -0800

    add dropdowns to navbar

--- client/src/components/Navbar/AppNav.js
+++ client/src/components/Navbar/AppNav.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Navbar, NavItem, Nav } from "react-bootstrap";
-
+import { Navbar, NavItem, Nav, MenuItem, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const AppNav = () => {
   return (
     <Navbar collapseOnSelect id="navbar">
@@ -14,17 +14,59 @@ const AppNav = () => {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
-          <NavItem id="Students" eventKey={1} href="/students/">
-            Students
+          <NavItem id="Students" eventKey={1}>
+            <NavDropdown eventKey={3} title="Students" id="basic-nav-dropdown">
+              <LinkContainer to="/students">
+                <MenuItem eventKey={3.1}>View Students</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/students">
+                <MenuItem eventKey={3.1}>Test Students</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/student-charts/words/">
+                <MenuItem eventKey={3.2}>View Student Word Charts</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/student-charts/letters/">
+                <MenuItem eventKey={3.3} href="/student-charts/letters/">
+                  View Student Letter Charts
+                </MenuItem>
+              </LinkContainer>
+
+              <LinkContainer to="/student-charts/sounds/">
+                <MenuItem eventKey={3.4} href="/student-charts/sounds/">
+                  View Student Sound Charts
+                </MenuItem>
+              </LinkContainer>
+            </NavDropdown>
           </NavItem>
-          <NavItem id="Words" eventKey={2} href="/words/">
-            Words
+          <NavItem id="Words" eventKey={2}>
+            <NavDropdown eventKey={3} title="Words" id="basic-nav-dropdown">
+              <LinkContainer to="/items/words/">
+                <MenuItem eventKey={3.1}>View Words</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/item-charts/words/">
+                <MenuItem eventKey={3.2}>View Word Charts</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
           </NavItem>
-          <NavItem id="Letters" eventKey={3} href="/letters/">
-            Letters
+          <NavItem id="Words" eventKey={2}>
+            <NavDropdown eventKey={3} title="Letters" id="basic-nav-dropdown">
+              <LinkContainer to="/items/words/">
+                <MenuItem eventKey={3.1}>View Letters</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/item-charts/letters/">
+                <MenuItem eventKey={3.2}>View Letter Charts</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
           </NavItem>
-          <NavItem id="Sounds" eventKey={4} href="/sounds/">
-            Sounds
+          <NavItem id="Words" eventKey={2}>
+            <NavDropdown eventKey={3} title="Sounds" id="basic-nav-dropdown">
+              <LinkContainer to="/items/words/">
+                <MenuItem eventKey={3.1}>View Sounds</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/item-charts/sounds/">
+                <MenuItem eventKey={3.2}>View Sound Charts</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
           </NavItem>
         </Nav>
         <Nav pullRight>
