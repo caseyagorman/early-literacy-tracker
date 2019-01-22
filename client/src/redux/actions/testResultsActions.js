85c969be8f3afe4ab8fd3322ccb2dc7e0d7123a7
Casey Gorman <caseyagorman@gmail.com>
2019-01-22 09:41:12 -0800

    add get all student test results action

--- client/src/redux/actions/testResultsActions.js
+++ client/src/redux/actions/testResultsActions.js
@@ -5,6 +5,10 @@ function getStudentTestResultsApi(itemType, id) {
   return `http://localhost:5000/api/get-student-item-test/${itemType}/${id}`;
 }
 
+function getStudentAllTestsResultsApi(id) {
+  return `http://localhost:5000/api/get-all-student-tests/${id}`;
+}
+
 export function receiveTestResults(testResults) {
   console.log("action", testResults);
   return { type: types.RECEIVE_TEST_RESULTS, testResults: testResults };
@@ -13,6 +17,22 @@ export function receiveTestResults(testResults) {
 export function fetchTestResults(user, itemType, id) {
   return dispatch => {
     return fetch(getStudentTestResultsApi(itemType, id), {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": user
+      }
+    })
+      .then(response => response.json())
+      .then(testResults => dispatch(receiveTestResults(testResults)));
+  };
+}
+
+export function fetchAllTestResults(user, id) {
+  return dispatch => {
+    return fetch(getStudentAllTestsResultsApi(id), {
       method: "GET",
       mode: "cors",
       headers: {
