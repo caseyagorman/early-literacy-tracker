0e438b2b0528fbc278623a449a5b09a5a81652af
Casey Gorman <caseyagorman@gmail.com>
2019-01-27 15:39:39 -0800

    add balloons

--- client/src/components/Students/StudentDetailMenu.js
+++ client/src/components/Students/StudentDetailMenu.js
@@ -1,4 +1,42 @@
 import React from "react";
 import "./static/students.css";
-const StudentDetailMenu = props => <div className="student-detail-menu" />;
+import image from "./static/squiggly-line.png";
+const StudentDetailMenu = props => (
+  <div className="student-detail-menu">
+    {" "}
+    <div className="word-balloons-top">
+      <div className="balloon" id="top-left">
+        <img id="top-left-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="top-center">
+        <img id="top-center-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="top-right">
+        <img id="top-right-img" src={image} alt="squiggly-line" />
+      </div>
+    </div>
+    <div className="word-balloons-center">
+      <div className="balloon" id="center-left">
+        <img id="center-left-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="center-center">
+        <img id="center-center-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="center-right">
+        <img id="center-right-img" src={image} alt="squiggly-line" />
+      </div>
+    </div>
+    <div className="word-balloons-bottom">
+      <div className="balloon" id="bottom-left">
+        <img id="bottom-left-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="bottom-center">
+        <img id="bottom-center-img" src={image} alt="squiggly-line" />
+      </div>
+      <div className="balloon" id="bottom-right">
+        <img id="bottom-right-img" src={image} alt="squiggly-line" />
+      </div>
+    </div>
+  </div>
+);
 export default StudentDetailMenu;
