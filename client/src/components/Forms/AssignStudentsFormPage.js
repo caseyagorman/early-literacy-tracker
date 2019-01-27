5bff11fcee2d429d23864e27ad08c743197393cb
Casey Gorman <caseyagorman@gmail.com>
2019-01-27 12:41:01 -0800

    rename dolch list files

---
+++ client/src/components/Forms/AssignStudentsFormPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+const AssignStudentsForm = props => (
+  <form onSubmit={props.handleSubmit} id="assign-items-form">
+    <FormGroup controlId="formControlsSelectMultiple">
+      <ControlLabel>
+        <h3>Assign students to "{props.item}"</h3>
+        <p>Press shift and click to add multiple students </p>
+      </ControlLabel>
+      <strong>
+        <FormControl
+          componentClass="select"
+          multiple
+          value={props.value}
+          onChange={props.handleChange}
+        >
+          {props.studentList.map(student => (
+            <option>{student.student}</option>
+          ))}
+        </FormControl>
+      </strong>
+    </FormGroup>
+    <button id="add-to-student-button" type="submit">
+      Submit
+    </button>
+  </form>
+);
+
+export default AssignStudentsForm;
