072aed864cf897624fff2aea5f14f88b5eb1ebeb
Casey Gorman <caseyagorman@gmail.com>
2019-01-27 23:20:27 -0800

    update font in item detail

--- client/src/components/Items/ItemDetailPage.js
+++ client/src/components/Items/ItemDetailPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BasicTablePage from "../Tables/BasicTablePage";
 const ItemDetailPage = props => (
-  <div className="container">
+  <div className="container" id="item-detail">
     <h1>{props.item.item.item}</h1>
 
     <BasicTablePage items={props.item} />
