9c2992a9b5bafa44217c2835092760a03dba3e75
Casey Gorman <caseyagorman@gmail.com>
2019-02-06 23:32:06 -0800

    add send directory

--- server/server.py
+++ server/server.py
@@ -8,7 +8,7 @@ import time
 import itertools
 import string
 from operator import itemgetter 
-from flask import Flask, render_template, jsonify, request, session, make_response
+from flask import Flask, render_template, jsonify, request, session, make_response, send_from_directory
 from jinja2 import StrictUndefined
 from flask_cors import CORS, cross_origin
 from flask_restful import  Api, Resource, reqparse
