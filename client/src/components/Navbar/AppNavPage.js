8618d05998f942fdf9fba911a87440b88a1a6f75
Casey Gorman <caseyagorman@gmail.com>
2019-02-08 21:25:06 -0800

    navbar test

--- client/src/components/Navbar/AppNavPage.js
+++ client/src/components/Navbar/AppNavPage.js
@@ -23,62 +23,36 @@ const AppNavPage = props => {
       <Navbar.Collapse>
         <Nav>
           <LinkContainer to="/students" id="Students">
-            <MenuItem id="Students">View Students</MenuItem>
+            <MenuItem id="Students">Students</MenuItem>
           </LinkContainer>
           <LinkContainer id="Students" to="/student-charts/words/">
-            <MenuItem id="Students">View Student Word Charts</MenuItem>
+            <MenuItem id="Students">Student Word Charts</MenuItem>
           </LinkContainer>
           <LinkContainer id="Students" to="/student-charts/letters/">
             <MenuItem id="Students" href="/student-charts/letters/">
-              View Student Letter Charts
+              Student Letter Charts
             </MenuItem>
           </LinkContainer>
           <LinkContainer id="Students" to="/student-charts/sounds/">
             <MenuItem id="Students" href="/student-charts/sounds/">
-              View Student Sound Charts
+              Student Sound Charts
             </MenuItem>
           </LinkContainer>
-          <LinkContainer id="Words" to="/items/words/">
-            <MenuItem id="Words">View Words</MenuItem>
-          </LinkContainer>
-          <LinkContainer id="Words" to="/item-charts/words/">
-            <MenuItem id="Words">View Word Charts</MenuItem>
-          </LinkContainer>
-          <LinkContainer id="Words" to="/add-items/words/">
-            <MenuItem id="Words">Add Words</MenuItem>
-          </LinkContainer>
-          <LinkContainer to="/items/letters/" id="Letters">
-            <MenuItem id="Letters">View Letters</MenuItem>
-          </LinkContainer>
-          <LinkContainer id="Letters" to="/item-charts/letters/">
-            <MenuItem id="Letters">View Letter Charts</MenuItem>
-          </LinkContainer>
-          <LinkContainer to="/add-items/letters/">
-            <MenuItem id="Letters">Add Letters</MenuItem>
-          </LinkContainer>
-          <LinkContainer to="/add-student" id="Students">
-            <MenuItem id="Students">Add Students</MenuItem>
-          </LinkContainer>
-
-          <LinkContainer to="/items/sounds/" id="Sounds">
-            <MenuItem id="Sounds">View Sounds</MenuItem>
-          </LinkContainer>
-          <LinkContainer to="/item-charts/sounds/" id="Sounds">
-            <MenuItem id="Sounds">View Sound Charts</MenuItem>
-          </LinkContainer>
-          <LinkContainer to="/add-items/sounds/">
-            <MenuItem id="Sounds">Add Sounds</MenuItem>
-          </LinkContainer>
-
           <LinkContainer to="/reading-level-charts/" id="Students">
-            <MenuItem id="Students">View Reading Level Charts</MenuItem>
+            <MenuItem id="Students">Reading Level Charts</MenuItem>
+          </LinkContainer>
+          <LinkContainer id="Words" to="/items/words/">
+            <MenuItem id="Words">Words</MenuItem>
           </LinkContainer>
 
-          <LinkContainer to="/create-group/" id="Students">
-            <MenuItem id="Students">Create Group</MenuItem>
+          <LinkContainer to="/items/letters/" id="Letters">
+            <MenuItem id="Letters">Letters</MenuItem>
+          </LinkContainer>
+          <LinkContainer to="/items/sounds/" id="Sounds">
+            <MenuItem id="Sounds">Sounds</MenuItem>
           </LinkContainer>
           <LinkContainer to="/manage-groups/" id="Students">
-            <MenuItem id="Students"> View/Manage Groups</MenuItem>
+            <MenuItem id="Students"> Manage Groups</MenuItem>
           </LinkContainer>
         </Nav>
         <Nav pullRight>
