25ac1dad23d971dad73fc461180f6f49ef00bd08
Casey Gorman <caseyagorman@gmail.com>
2019-02-09 14:12:00 -0800

    update test student formatting

--- client/src/components/TestStudent/StudentTestPage.js
+++ client/src/components/TestStudent/StudentTestPage.js
@@ -1,63 +1,70 @@
 import React from "react";
 import "./static/test.css";
-
+import { Row } from "react-bootstrap";
 const StudentTestPage = props => (
-  <div className="container">
+  <div
+    className="container"
+    style={{
+      textAlign: "center"
+    }}
+  >
     <div
       style={{
+        textAlign: "center",
         marginTop: "5%",
-        marginLeft: "25%",
-        fontSize: 200,
+        fontSize: 300,
         fontFamily: "krub",
         color: "black"
       }}
     >
-      {props.displayItem(props.studentTestItems[props.idx])}
+      <Row>{props.displayItem(props.studentTestItems[props.idx])}</Row>
     </div>
-    <button
-      style={{
-        width: 100,
-        borderRadius: 4,
-        fontSize: 24,
-        fontFamily: "krub",
-        marginLeft: "20%",
-        padding: 5,
-        backgroundColor: "#44857d",
-        color: "white"
-      }}
-      onClick={e =>
-        props.handleTestClick(e, props.studentTestItems[props.idx], props.idx)
-      }
-      value="yes"
-    >
-      Yes
-    </button>
-    <button
-      style={{
-        width: 100,
-        borderRadius: 4,
-        fontSize: 24,
-        fontFamily: "krub",
-        marginLeft: "25%",
-        padding: 5,
-        backgroundColor: "#FE6625",
-        color: "white"
-      }}
-      onClick={e =>
-        props.handleTestClick(e, props.studentTestItems[props.idx], props.idx)
-      }
-      value="no"
-    >
-      No
-    </button>
+    <Row>
+      <button
+        style={{
+          width: 100,
+          borderRadius: 4,
+          fontSize: 24,
+          fontFamily: "krub",
+          padding: 5,
+          backgroundColor: "#44857d",
+          color: "white"
+        }}
+        onClick={e =>
+          props.handleTestClick(e, props.studentTestItems[props.idx], props.idx)
+        }
+        value="yes"
+      >
+        Yes
+      </button>
+      <button
+        style={{
+          width: 100,
+          borderRadius: 4,
+          fontSize: 24,
+          fontFamily: "krub",
+          marginLeft: "25%",
+          padding: 5,
+          backgroundColor: "#FE6625",
+          color: "white"
+        }}
+        onClick={e =>
+          props.handleTestClick(e, props.studentTestItems[props.idx], props.idx)
+        }
+        value="no"
+      >
+        No
+      </button>
+    </Row>
     <button
       onClick={e => props.endTest(e)}
       style={{
         fontFamily: "krub",
+        height: 46,
         width: 150,
         borderRadius: 4,
         fontSize: 24,
-        marginLeft: "35%",
+
         backgroundColor: "rgb(0, 61, 89, 0.7)",
         color: "white"
       }}
