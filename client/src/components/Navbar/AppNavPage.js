2adbe661deb231ea3dce27292d42fae63bc1f15a
Casey Gorman <caseyagorman@gmail.com>
2019-02-08 21:10:42 -0800

    navbar back to original

--- client/src/components/Navbar/AppNavPage.js
+++ client/src/components/Navbar/AppNavPage.js
@@ -1,34 +1,109 @@
 import React from "react";
-import { Navbar, NavItem, Nav, Glyphicon } from "react-bootstrap";
-// import { Nav.Link } from "react-router-bootstrap";
+import {
+  Navbar,
+  NavItem,
+  Nav,
+  MenuItem,
+  NavDropdown,
+  Glyphicon
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./static/appNav.css";
-
 const AppNavPage = props => {
   return (
-    <Navbar collapseOnSelect expand="lg">
+    <Navbar collapseOnSelect id="navbar">
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="/students">TrackIt</a>
+          <a href="/students" id="Brand">
+            TrackIt
+          </a>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
-          <Nav.Link href="/student-charts/words/">Student Word Charts</Nav.Link>
-          <Nav.Link href="/student-charts/letters/">
-            Student Letter Charts
-          </Nav.Link>
-          <Nav.Link href="/student-charts/sounds/">
-            Student Sound Charts
-          </Nav.Link>
-          <Nav.Link href="/reading-level-charts/">Reading Charts</Nav.Link>
-          <Nav.Link href="/items/words/">Words</Nav.Link>
-          <Nav.Link href="/items/letters/">Letters</Nav.Link>
-          <Nav.Link href="/items/sounds/">Sounds</Nav.Link>
-          <Nav.Link href="/manage-groups/">Groups</Nav.Link>
+          <NavItem id="Students" eventKey={1}>
+            <NavDropdown eventKey={3} title="Students" id="drop-down">
+              <LinkContainer to="/students" id="Students">
+                <MenuItem id="Students">View Students</MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Students" to="/student-charts/words/">
+                <MenuItem id="Students">View Student Word Charts</MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Students" to="/student-charts/letters/">
+                <MenuItem id="Students" href="/student-charts/letters/">
+                  View Student Letter Charts
+                </MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Students" to="/student-charts/sounds/">
+                <MenuItem id="Students" href="/student-charts/sounds/">
+                  View Student Sound Charts
+                </MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
+          <NavItem id="Words" eventKey={2}>
+            <NavDropdown title="Words" id="drop-down">
+              <LinkContainer id="Words" to="/items/words/">
+                <MenuItem id="Words">View Words</MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Words" to="/item-charts/words/">
+                <MenuItem id="Words">View Word Charts</MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Words" to="/add-items/words/">
+                <MenuItem id="Words">Add Words</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
+          <NavItem id="Letters" eventKey={2}>
+            <NavDropdown eventKey={3} title="Letters" id="drop-down">
+              <LinkContainer to="/items/letters/" id="Letters">
+                <MenuItem id="Letters">View Letters</MenuItem>
+              </LinkContainer>
+              <LinkContainer id="Letters" to="/item-charts/letters/">
+                <MenuItem id="Letters">View Letter Charts</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/add-items/letters/">
+                <MenuItem id="Letters">Add Letters</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/add-student" id="Students">
+                <MenuItem id="Students">Add Students</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
+          <NavItem id="Sounds">
+            <NavDropdown title="Sounds" id="drop-down">
+              <LinkContainer to="/items/sounds/" id="Sounds">
+                <MenuItem id="Sounds">View Sounds</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/item-charts/sounds/" id="Sounds">
+                <MenuItem id="Sounds">View Sound Charts</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/add-items/sounds/">
+                <MenuItem id="Sounds">Add Sounds</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
+          <NavItem id="Students">
+            <NavDropdown title="Reading" id="drop-down">
+              <LinkContainer to="/reading-level-charts/" id="Students">
+                <MenuItem id="Students">View Reading Level Charts</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
+          <NavItem id="Students">
+            <NavDropdown title="Groups" id="drop-down">
+              <LinkContainer to="/create-group/" id="Students">
+                <MenuItem id="Students">Create Group</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/manage-groups/" id="Students">
+                <MenuItem id="Students"> View/Manage Groups</MenuItem>
+              </LinkContainer>
+            </NavDropdown>
+          </NavItem>
         </Nav>
         <Nav pullRight>
-          <NavItem href={props.route}>
+          <NavItem id="Logout" eventKey={5} href={props.route}>
             {props.navText} <span />
             <Glyphicon
               glyph="glyphicon glyphicon-off"
