e14e91972f6af1715e61bf4724e0c9c33fda373c
vagrant <vagrant@vagrant.vm>
2019-02-18 22:57:08 +0000

    include script to create all necessary tables

--- server/init_db.py
+++ server/init_db.py
@@ -2,7 +2,7 @@ import subprocess
 import model
 from server import app
 
-subprocess.run(["createdb", "students"])
+# subprocess.run(['dropdb', 'students'])
+# subprocess.run(["createdb", "students"])
 model.connect_to_db(app)
 model.db.create_all()
-
