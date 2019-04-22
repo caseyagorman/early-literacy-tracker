ef264df665a128cf3d79a7fef079008ec0fbf5c0
Casey Gorman <caseyagorman@gmail.com>
2019-04-21 18:01:40 -0700

    add devtools and server setup scripts

---
+++ src/server/server-setup.sh
@@ -0,0 +1,6 @@
+# Using set -e will make the script exit if any line gives as non-zero return
+set -e
+python3 -m virtualenv env
+source env/bin/activate
+pip3 install -r requirements.txt
+python trackit.py
